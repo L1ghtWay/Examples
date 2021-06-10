c69758728d1324fda5ef659b45770c5281049c62
Fomenko Vladislav <ex.lightway@gmail.com>
2021-06-10 18:32:52 +0300

    Code for examples

---
+++ Approach.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Approach : MonoBehaviour
+{
+    private Vector3 savePos;
+    private Quaternion saveRot;    
+    private bool oneClick;
+    private bool inCenter = false;
+
+    private float timeForDoubleClick;
+    private float speedRotateX = 150;
+    private float speedRotateY = 150;
+
+    [System.Obsolete]
+    private void Update()
+    {
+        if (inCenter)
+        {
+            if (!Input.GetMouseButton(0))
+                return;
+
+            float rotX = Input.GetAxis("Mouse X") * speedRotateX * Time.deltaTime;
+            float rotY = Input.GetAxis("Mouse Y") * speedRotateY * Time.deltaTime;
+
+            if (Mathf.Abs(rotX) > Mathf.Abs(rotY))
+            {
+                transform.Rotate(transform.up, -rotX);
+            }
+            else
+            {
+                transform.Rotate(transform.right, -rotY);
+            }
+        }
+    }
+    
+#if UNITY_ANDROID
+
+#endif
+
+#if UNITY_EDITOR
+    private void OnMouseDown()
+    {       
+        if (!oneClick)
+        {
+            oneClick = true;
+            timeForDoubleClick = Time.time;
+            Debug.Log("первый клик");
+        }
+        else
+        {
+            if (Time.time - timeForDoubleClick > 0.3f)
+            {
+                timeForDoubleClick = Time.time;
+            }
+            else
+            {
+                Debug.Log("второй клик");
+                if (!inCenter) ApproachCube();
+                else RemoveCube();
+                oneClick = false;
+            }
+        }
+
+        if (!inCenter)
+        {
+            savePos = transform.position;
+            saveRot = transform.rotation;
+        }        
+    }
+#endif
+
+
+    private void ApproachCube ()
+    {
+        if (!LevelGenerator.Instance.CubeInCenter)
+        {
+            transform.position = new Vector3(0, 2, 2);
+            inCenter = true;
+            LevelGenerator.Instance.CubeInCenter = true;
+            LevelGenerator.Instance.StopSwipe(); // Останавлием возможность передигать кубы при приближении одного из них
+        }
+    }
+
+
+    private void RemoveCube ()
+    {
+        transform.position = savePos;
+        transform.rotation = saveRot;
+        inCenter = false;
+        LevelGenerator.Instance.CubeInCenter = false;
+        LevelGenerator.Instance.StartSwipe();                
+    }
+}
---
+++ ReturnCubePosition.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReturnCubePosition : MonoBehaviour
+{
+    [SerializeField] float moveTime = 2f;
+    private List<PointInTime> pointsInTime;
+    private bool isRewinding = false;
+    private bool isTargeting = false;
+    private Rigidbody rb;
+    private int index;
+
+    void Start()
+    {
+        pointsInTime = new List<PointInTime>();
+        rb = GetComponent<Rigidbody>();
+        index = GetComponent<Swipe>().Index;
+    }
+
+    
+    void Update()
+    {
+        if (LevelGenerator.Instance.CubeIsReturning)
+        {
+            LevelGenerator.Instance.CubeIsReturning = false;
+            StartRewind();
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if(isRewinding)
+        {
+            Rewind();
+        } 
+        
+        else if (isTargeting)
+        {
+            Record();
+        }
+    }
+
+
+    private void OnMouseDown()
+    {
+        isTargeting = true;
+    }
+
+
+    void Record()
+    {
+        if (pointsInTime.Count < Mathf.Round(moveTime / Time.fixedDeltaTime) && index == LevelGenerator.Instance.LastCubeIndex)
+        {
+            pointsInTime.Insert(0, new PointInTime(transform.position, transform.rotation));
+        }
+        
+    }
+
+    void Rewind()
+    {
+        if(pointsInTime.Count > 0)
+        {
+            PointInTime pointInTime = pointsInTime[0];
+            transform.position = pointInTime.position;
+            transform.rotation = pointInTime.rotation;
+            pointsInTime.RemoveAt(0);
+        }
+        else
+        {
+            StopRewinding();
+        }
+
+    }
+    
+    void StartRewind()
+    {
+        if (index == LevelGenerator.Instance.LastCubeIndex)
+        {
+            isRewinding = true;
+        }
+        rb.isKinematic = true;
+    }
+
+    void StopRewinding()
+    {
+        isRewinding = false;
+        rb.isKinematic = false;        
+    }
+}
